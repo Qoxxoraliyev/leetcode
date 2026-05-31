01efc7a95a9f3bd03b7e5d0051d2161238198dd8
Muhammadali <qoxxoraliyev.muhammadali2006@gmail.com>
2026-05-31 18:43:28 +0500

    initial commit

---
+++ NumberOf1Bits.java
@@ -0,0 +1,22 @@
+public class NumberOf1Bits {
+
+
+    public static int hammingWeight(int n){
+
+        int count=0;
+        for (int i = 0; i <32; i++) {
+            if ((n&1)==1){
+                count++;
+            }
+            n=n>>1;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(hammingWeight(5));
+
+    }
+}
--- ReverseBits.java
+++ ReverseBits.java
@@ -1,2 +1,22 @@
 public class ReverseBits {
+
+
+    public static int reverseBits(int n){
+
+        int result=0;
+        for (int i = 0; i <32; i++) {
+            int bit=n&1;
+            n=n>>1;
+            result=result<<1;
+            result=result | bit;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(reverseBits(5));
+
+    }
 }
