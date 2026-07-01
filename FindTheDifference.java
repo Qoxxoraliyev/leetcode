a3f79b29092524225212ecac08c708070dffc95a
Muhammadali <qoxxoraliyev.muhammadali2006@gmail.com>
2026-07-01 22:53:49 +0500

    initial commit

---
+++ FindTheDifference.java
@@ -0,0 +1,11 @@
+public class FindTheDifference {
+
+
+    public static char findTheDifference(String s,String t){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- FirstUniqueCharacterInAString.java
+++ FirstUniqueCharacterInAString.java
@@ -1,2 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class FirstUniqueCharacterInAString {
+
+    public static int firstUniqueChar(String s){
+
+        Map<Character,Integer> map=new HashMap<>();
+        for (int i = 0; i <s.length(); i++) {
+            if (!map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i),1);
+            }
+            else {
+                map.put(s.charAt(i),map.get(s.charAt(i))+1);
+            }
+        }
+
+        for (int i = 0; i <s.length(); i++) {
+            if (map.containsKey(s.charAt(i)) && map.get(s.charAt(i))==1){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println(firstUniqueChar("leetcode")); // 0
+
+    }
 }
